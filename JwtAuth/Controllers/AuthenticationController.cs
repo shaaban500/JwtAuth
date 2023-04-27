6592584a42497491cc6aee3c2491d1cd2c048549
Mohamed Shaaban <moshaaban500@gmail.com>
2023-04-27 11:19:39 +0200

    Adding Register endpoint

--- JwtAuth/Controllers/AuthenticationController.cs
+++ JwtAuth/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
 ﻿using JwtAuth.Configurations;
+using JwtAuth.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,67 @@ namespace JwtAuth.Controllers
             _jwtConfig = jwtConfig;
             _userManager = userManager;
         }
+
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterationRequestDto requestDto)
+        {
+            // validate incoming request
+            if(ModelState.IsValid)
+            {
+                var userExist = await _userManager.FindByEmailAsync(requestDto.Email);
+                
+                if(userExist != null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Result = false,
+                        Errors = new List<string>()
+                        {
+                            "Email is already exist"
+                        }
+                    });
+                }
+
+                // create a new user
+                var newUser = new IdentityUser()
+                {
+                    Email = requestDto.Email,
+                    UserName = requestDto.Email
+                };
+
+
+                var isCreated = await _userManager.CreateAsync(newUser, requestDto.Password);
+
+                if(isCreated.Succeeded)
+                {
+                    // Generate the token
+                }
+
+                return BadRequest(new AuthResult() 
+                {
+                    Result = false,
+                    Errors = new List<string>()
+                    {
+                        "Server Error"
+                    }
+                });
+
+            }
+            return BadRequest();
+        }
+
+
+
+
+
+        // Generate token
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            return "";
+        }
+
 
     }
 }
